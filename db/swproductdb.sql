96995c3ad1bef6efd23b9fb6c2483fa842268b1e
Sefanovskis-Artjoms <56579832+Sefanovskis-Artjoms@users.noreply.github.com>
2021-09-21 01:38:39 +0300

    changed type column

--- db/swproductdb.sql
+++ db/swproductdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 11, 2021 at 02:50 PM
+-- Generation Time: Sep 21, 2021 at 12:35 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -37,7 +37,7 @@ CREATE TABLE `products` (
   `width` int(11) DEFAULT NULL,
   `height` int(11) DEFAULT NULL,
   `lenght` int(11) DEFAULT NULL,
-  `type` int(11) NOT NULL
+  `type` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -45,18 +45,19 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`ID`, `sku`, `name`, `price`, `size`, `weight`, `width`, `height`, `lenght`, `type`) VALUES
-(1, 'JVC09230984', 'ACME DISC', 1, 700, NULL, NULL, NULL, NULL, 1),
-(2, 'JVC09230984', 'ACME DISC', 1, 700, NULL, NULL, NULL, NULL, 1),
-(3, 'JVC09230984', 'ACME DISC', 1, 700, NULL, NULL, NULL, NULL, 1),
-(4, 'JVC09230984', 'ACME DISC', 1, 700, NULL, NULL, NULL, NULL, 1),
-(5, 'GGW903284', 'War and peace', 20, NULL, 2, NULL, NULL, NULL, 2),
-(6, 'GGW903284', 'War and peace', 20, NULL, 2, NULL, NULL, NULL, 2),
-(7, 'GGW903284', 'War and peace', 20, NULL, 2, NULL, NULL, NULL, 2),
-(8, 'GGW903284', 'War and peace', 20, NULL, 2, NULL, NULL, NULL, 2),
-(9, 'TR92387492', 'Chair', 50, NULL, NULL, 45, 25, 15, 3),
-(10, 'TR92387492', 'Chair', 50, NULL, NULL, 45, 25, 15, 3),
-(11, 'TR92387492', 'Chair', 50, NULL, NULL, 45, 25, 15, 3),
-(12, 'TR92387492', 'Chair', 50, NULL, NULL, 45, 25, 15, 3);
+(1, 'JVC09230984', 'ACME DISC', 1, 700, NULL, NULL, NULL, NULL, 'DVD'),
+(2, 'JVC09230984', 'ACME DISC', 1, 700, NULL, NULL, NULL, NULL, 'DVD'),
+(3, 'JVC09230984', 'ACME DISC', 1, 700, NULL, NULL, NULL, NULL, 'DVD'),
+(4, 'JVC09230984', 'ACME DISC', 1, 700, NULL, NULL, NULL, NULL, 'DVD'),
+(5, 'GGW903284', 'War and peace', 20, NULL, 2, NULL, NULL, NULL, 'Book'),
+(6, 'GGW903284', 'War and peace', 20, NULL, 2, NULL, NULL, NULL, 'Book'),
+(7, 'GGW903284', 'War and peace', 20, NULL, 2, NULL, NULL, NULL, 'Book'),
+(8, 'GGW903284', 'War and peace', 20, NULL, 2, NULL, NULL, NULL, 'Book'),
+(9, 'TR92387492', 'Chair', 50, NULL, NULL, 45, 25, 15, 'Furniture'),
+(10, 'TR92387492', 'Chair', 50, NULL, NULL, 45, 25, 15, 'Furniture'),
+(30, 'aa', 'aa', 1, 0, 1, 0, 0, 0, 'Book'),
+(31, 'bbb', 'disc', 111, 500, 0, 0, 0, 0, 'DVD'),
+(32, 'cccc', 'furniture', 222, 0, 0, 2, 1, 3, 'Furniture');
 
 --
 -- Indexes for dumped tables
@@ -76,9 +77,10 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
